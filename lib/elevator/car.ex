7346ce943f2e54e127f2b771ce2ecd1861c8140c
Greg Vaughn <gvaughn@gmail.com>
2013-10-19 13:48:17 -0500

    timeout callback working

--- lib/elevator/car.ex
+++ lib/elevator/car.ex
@@ -2,15 +2,24 @@ defmodule Elevator.Car do
   #use OtpDsl.Genserver
   use GenServer.Behaviour
 
+  @timeout 1000
+
   def start_link() do
     :gen_server.start_link({:local, :car1}, __MODULE__, [], [])
+    # can't just use :car1 once we have multiple instances of Elevator.Car
   end
 
   def init(state) do
-    # this 3rd value is a timeout in millis before a handle_info(:timeout, state) callback is called
-    {:ok, state, 1000}
+    {:ok, state, @timeout}
   end
 
   def hi(), do: IO.inspect 'hello'
+
+  def handle_info(:timeout, state) do
+    IO.puts "timeout called with state: #{state}"
+    {:noreply, state, @timeout}
+    #TODO we might want the timeout for cast and calls so that it mimics the doors waiting
+    # to close after floor selection
+  end
 
 end
