58764fb44ee1824e7fcc63f6ac664e587a362fd5
Greg Vaughn <gvaughn@gmail.com>
2013-10-20 14:44:23 -0500

    move num_elevators into mix.exs

--- lib/elevator.ex
+++ lib/elevator.ex
@@ -3,8 +3,8 @@ defmodule Elevator do
 
   # See http://elixir-lang.org/docs/stable/Application.Behaviour.html
   # for more information on OTP Applications
-  def start(_type, _args) do
-    Elevator.Supervisor.start_link(2)
-    # TODO parametrize num_cars (2) into .app file
+  #def start(_type, _args) do
+  def start(_type, [num_cars]) do
+    Elevator.Supervisor.start_link(num_cars)
   end
 end
--- mix.exs
+++ mix.exs
@@ -1,6 +1,8 @@
 defmodule Elevator.Mixfile do
   use Mix.Project
 
+  @num_elevators 2
+
   def project do
     [ app: :elixir_elevated,
       version: "0.0.1",
@@ -10,7 +12,7 @@ defmodule Elevator.Mixfile do
 
   # Configuration for the OTP application
   def application do
-    [mod: { Elevator, [] }]
+    [mod: { Elevator, [@num_elevators] }]
   end
 
   # Returns the list of dependencies in the format:
