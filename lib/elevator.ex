12eeed869c0f4262be813cf1e41de975fad79063
Greg Vaughn <gvaughn@gmail.com>
2013-10-20 15:17:50 -0500

    prep for HallMonitor

--- lib/elevator.ex
+++ lib/elevator.ex
@@ -1,9 +1,6 @@
 defmodule Elevator do
   use Application.Behaviour
 
-  # See http://elixir-lang.org/docs/stable/Application.Behaviour.html
-  # for more information on OTP Applications
-  #def start(_type, _args) do
   def start(_type, [num_cars]) do
     Elevator.Supervisor.start_link(num_cars)
   end
--- lib/elevator/supervisor.ex
+++ lib/elevator/supervisor.ex
@@ -3,23 +3,24 @@ defmodule Elevator.Supervisor do
 
   def start_link(num_cars) do
     # num_cars comes from Elevator.start Application
-    # this may get more complex if I have to start a HallMonitor and pass its pid to the Cars
-    # see iPad p#597 of Programming Elixir 17.1 subsection: Managing Process State Across Restarts
-    :supervisor.start_link(__MODULE__, [num_cars])
+    # see Programming Elixir 17.1 subsection: Managing Process State Across Restarts
+    result = {:ok, sup} = :supervisor.start_link(__MODULE__, [])
     # 2nd param of start_link passed to init by supervisor
+    start_workers(sup, num_cars)
+    result
   end
 
-  def init([num_cars]) do
-    #children = [
-      # Define workers and child supervisors to be supervised
-      # worker(Elevator.Car, [])
-      #]
-    # need to use a more complex version so module is not used as id or else no duplicates
-    children = Enum.map(1..num_cars, &(worker(Elevator.Car, [&1], [id: "Elevator.Car-#{&1}"])))
-    #IO.inspect children
+  def init(_) do
+    # init is expected to return child_specs
+    # since we need to provide HallMonitor pid to the Cars, defer
+    supervise([], strategy: :one_for_one)
+  end
 
-    # See http://elixir-lang.org/docs/stable/Supervisor.Behaviour.html
-    # for other strategies and supported options
-    supervise(children, strategy: :one_for_one)
+  def start_workers(sup, num_cars) do
+    # start the HallMonitor here
+    # {:ok, hm} = :supervisor.start_child
+    Enum.map(1..num_cars, &(
+      :supervisor.start_child(sup, worker(Elevator.Car, [&1], [id: "Elevator.Car-#{&1}"]))
+    ))
   end
 end
