4c95b25dcc4df9de9c9b666742fc95d4a2edde24
Greg Vaughn <gvaughn@gmail.com>
2014-06-11 21:00:17 -0500

    test for timeout tick behavior of Car

--- test/elevator/car_test.exs
+++ test/elevator/car_test.exs
@@ -1,10 +1,23 @@
-module Elevatar.CarTest do
-  use ExUnit.Case
+defmodule Elevatar.CarTest do
+  use ExUnit.Case #,async: true
   alias Elevator.Car
 
-  #TODO setup/teardown like unpublished tutorial on elixir-lang.org
-  test "blah" do
+  setup do
+    {:ok, test_event} = GenEvent.start_link(name: :test_event)
+    #TODO pass in the name to HallSignal.start_link
+    {:ok, car} = Elevator.Car.start_link({1, :test_event, :hall_signal, :infinity})
+    #on_exit(fn -> Elevator.Car.stop(car) end) #TODO teardown properly
+    {:ok, car: car}
+  end
+
+  test "2 ticks arrives at floor 2", %{car: car} do
     Car.go_to(car, 2, self)
+    tick(car)
+    tick(car)
     assert_receive {:arrival, 2, ^car}
+  end
+
+  defp tick(car) do
+    send(car, :timeout)
   end
 end
