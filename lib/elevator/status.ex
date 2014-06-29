657340ebb79f52bb3f2e09ea6c8fcb8859ca4bbc
Greg Vaughn <gvaughn@gmail.com>
2014-06-29 16:14:27 -0500

    animation sample

--- lib/elevator/status.ex
+++ lib/elevator/status.ex
@@ -31,6 +31,10 @@ defmodule Elevator.Status do
 end
 
 defmodule Temp do
+  @num_floors 9
+  @floor_height 3
+  @building_x 20
+  @building_y 10
   @car_with_rider """
   ╭┻┻╮
   ┃😁 ║
@@ -43,30 +47,46 @@ defmodule Temp do
   """
   @car_sprite [
    "╭┻┻╮",
-   "║😁 ║",
+   "║😃 ║",
    "┗━━┛"
    ]
-
+  @car_width String.length(List.first(@car_sprite))
   @floor "\x{2560}" <> String.duplicate("\x{2550}", 10) <> "\x{2563}"
   @roof "\x{2560}" <> String.duplicate("\x{2568}", 10) <> "\x{2563}"
+  @building_width String.length(@roof)
 
-  def wall, do: "\x{254E}" <> str(10) <> "\x{254E}"
-  def wall(num), do: "\x{2551}" <> str(4) <> "#{num}\x{20E3}" <> str(5) <> "\x{2551}"
-
-  def floor(num), do: [wall(num), wall, @floor]
-
-  @num_floors 9
-  def draw do
-    static = building(@num_floors) |> pos_at(20, 10)
-    car = @car_sprite |> pos_at(17, 17)
-    car2 = @car_sprite |> pos_at(31, 32)
-    IO.puts [static, car, car2]
+  def run do
+    hide_cursor
+    draw [car(:left, 7), car(:right, 2)]
+    draw [car(:left, 6), car(:right, 3)]
+    draw [car(:left, 5), car(:right, 4)]
+    draw [car(:left, 4), car(:right, 5)]
+    draw [car(:left, 3), car(:right, 6)]
+    draw [car(:left, 2), car(:right, 7)]
     IO.puts pos_at("", 1, 40) #move prompt out of way
+    show_cursor
   end
 
-  #TODO function to get car's x,y given left/right and floor
+  defp draw(sprites) do
+    clear_screen
+    static = building(@num_floors) |> pos_at(@building_x, @building_y)
+    IO.write [static | sprites]
+    :timer.sleep 500
+  end
 
-  def building(count) do
+  defp car(:left, floor) do
+    car_x = 1 + @building_x - @car_width
+    car_y = 1 + @building_y + (@num_floors - floor) * @floor_height
+    pos_at(@car_sprite, car_x, car_y)
+  end
+
+  defp car(:right, floor) do
+    car_x = @building_x + @building_width - 1
+    car_y = 1 + @building_y + (@num_floors - floor) * @floor_height
+    pos_at(@car_sprite, car_x, car_y)
+  end
+
+  defp building(count) do
     [@roof | count..1 |> Enum.map(&floor(&1)) |> List.flatten ]
   end
 
@@ -78,5 +98,15 @@ defmodule Temp do
   end
   defp pos_at(str, x, y), do: "\e[#{y};#{x}H#{str}"
 
+  defp clear_screen, do: IO.write "\e[2J"
+  defp hide_cursor,  do: IO.write "\e[?25l"
+  defp show_cursor,  do: IO.write "\e[?25h"
+
   defp str(count), do: String.duplicate(" ", count)
+
+  defp wall, do: "\x{254E}" <> str(10) <> "\x{254E}"
+  defp wall(num), do: "\x{2551}" <> str(4) <> "#{num}\x{20E3}" <> str(5) <> "\x{2551}"
+
+  defp floor(num), do: [wall(num), wall, @floor]
+
 end
